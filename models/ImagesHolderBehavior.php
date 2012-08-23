af5bdd0d2b45b6b5fe24cc862ccb989c3791ce47
alari <name.alari@gmail.com>
2012-08-23 10:24:38 +0400

    added form mixin support

--- models/ImagesHolderBehavior.php
+++ models/ImagesHolderBehavior.php
@@ -3,7 +3,7 @@
  * @author alari
  * @since 8/21/12 3:16 PM
  */
-class ImagesHolderBehavior extends CActiveRecordBehavior
+class ImagesHolderBehavior extends CActiveRecordBehavior implements IFormMixin
 {
     public function afterSave($event) {
         /* @var $model ImagesHolderModel */
@@ -23,6 +23,35 @@ class ImagesHolderBehavior extends CActiveRecordBehavior
         foreach ($model->imageHolders() as $field => $type) {
             $holder = ImagesHolder::model()->findByPk($model->$field ? : $holdersCreated[$field]);
             if ($holder) $holder->setImagesFromPost();
+        }
+    }
+
+    public function onFormInit(ExtForm $form) {
+        $form->htmlOptions["enctype"] = 'multipart/form-data';
+    }
+
+    public function beforeFormEnd(ExtForm $form)
+    {
+    }
+
+    public function formInjectInside(ExtForm $form)
+    {
+        if($form->model instanceof ImagesHolderModel) {
+            /** @var $model ImagesHolderModel */
+            $model = $form->model;
+            $imageHolders = array();
+            foreach($model->imageHolders() as $h=>$t) {
+                $h = str_replace(" ", "", ucwords(str_replace("_", " ", substr($h, 0, -3))));
+                $h[0] = strtolower($h[0]);
+                $imageHolders[$h] = $t;
+            }
+            if(count($imageHolders)){?>
+            <fieldset>
+                <?foreach($imageHolders as $field=>$type) Yii::app()->controller->widget("imagesHolder.widgets.heldImages.EditImages", array("holder"=>(($model && $model->$field) ? $model->$field : $type))) ?>
+            </fieldset>
+            <?}
+        } else {
+            echo "You have to implement ImagesHolderModel on your model class";
         }
     }
 }
